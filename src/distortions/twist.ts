d850ee50da88dcb27ef1e64628c1ee41420b695a
Ben Jack <ben.jack@vuw.ac.nz>
2018-02-19 11:38:17 +1300

    added cup to twist

--- src/distortions/twist.ts
+++ src/distortions/twist.ts
@@ -16,7 +16,8 @@ export class Twist extends BufferDistortion{
 
     let d = new Vector3(position.x, 0, position.z).length();
 
-    let twist = this["twist"] * Math.pow(d, this["curve"]);//Math.pow(d, this["pow"]);
+    let shape = (Math.cos((1.0 - Math.min(Math.abs(uv.y-this["centre"])*2* (2.1-this["cupWidth"]), 1.0)) * 3.14159)+1)/2.0;
+    let twist = this["twist"] * Math.pow(d, this["curve"]) * (shape+d/this["bulge"]);//Math.pow(d, this["pow"]);
     let theta = Math.atan2(position.z, position.x) + twist;
 
     return new Vector3(Math.cos(theta)*d, position.y, Math.sin(theta)*d);
@@ -44,6 +45,42 @@ let UIDefinition:any = {
       "type":"range",
         "min":0.01,
         "max":3.0,
+        "value":1,
+        "step":0.001
+    }
+  },
+  "centrePoint":{
+    "variable":"centre",
+    "label":"centre point",
+    "listener":"input",
+    "attributes":{
+      "type":"range",
+        "min":0.0,
+        "max":1.0,
+        "value":0.5,
+        "step":0.001
+    }
+  },
+  "cupWidth":{
+    "variable":"cupWidth",
+    "label":"cup width",
+    "listener":"input",
+    "attributes":{
+      "type":"range",
+        "min":0.1,
+        "max":2.0,
+        "value":1,
+        "step":0.001
+    }
+  },
+  "cupBulge":{
+    "variable":"bulge",
+    "label":"cup bulge",
+    "listener":"input",
+    "attributes":{
+      "type":"range",
+        "min":1.0,
+        "max":10.0,
         "value":1,
         "step":0.001
     }
